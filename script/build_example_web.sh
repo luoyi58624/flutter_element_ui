e324b242e97a38c807812a52fdfbc917ddf368d9
luoyi <luoyi58624@gmail.com>
2024-05-04 21:09:44 +0800

    chore: web renderer使用canvaskit

--- script/build_example_web.sh
+++ script/build_example_web.sh
@@ -2,4 +2,4 @@ flutter --version
 flutter pub get
 # shellcheck disable=SC2164
 cd ./example
-flutter build web --web-renderer=html --pwa-strategy none --base-href /flutter_element_ui/
+flutter build web --web-renderer=canvaskit --pwa-strategy none --base-href /flutter_element_ui/
