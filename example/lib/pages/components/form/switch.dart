a108741d103fc52db7a8649aff66d5c44b0c4d30
luoyi <luoyi58624@163.com>
2024-06-27 10:37:09 +0800

    feat: 完成switch基础功能

--- example/lib/pages/components/form/switch.dart
+++ example/lib/pages/components/form/switch.dart
@@ -1,6 +1,6 @@
 import 'package:example/global.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 
 class SwitchPage extends HookWidget {
   const SwitchPage({super.key});
@@ -8,15 +8,139 @@ class SwitchPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final flag = useState(false);
-    return Center(
-      child: Column(
-        children: [
-          Text("Hello").animate().fade(),
-          ElSwitch(
-            value: flag.value,
-            onChanged: (v) => flag.value = v,
-          ),
-        ],
+    final disabled = useState(true);
+    const count = 100;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                ElSwitch(
+                  value: flag.value,
+                  onChanged: (v) => flag.value = v,
+                ),
+                const Gap(8),
+                ElSwitch(
+                  value: flag.value,
+                  width: 100,
+                  onChanged: (v) => flag.value = v,
+                ),
+                const Gap(8),
+                ElSwitch(
+                  value: flag.value,
+                  onChanged: (v) => flag.value = v,
+                  gap: 8,
+                ),
+                const Gap(8),
+                ElSwitch(
+                  value: disabled.value,
+                  onChanged: (v) => disabled.value = v,
+                ),
+                const Gap(8),
+                ElSwitch(
+                  value: flag.value,
+                  disabled: disabled.value,
+                  onChanged: (v) => flag.value = v,
+                ),
+              ],
+            ),
+            const _SwitchTest1(count),
+            const _SwitchTest2(count),
+            const _SwitchTest3(count),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SwitchTest1 extends HookWidget {
+  const _SwitchTest1(this.count);
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    final flag = useState(false);
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ...List.generate(
+              count,
+              (index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElSwitch(
+                  value: flag.value,
+                  onChanged: (v) => flag.value = v,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SwitchTest2 extends HookWidget {
+  const _SwitchTest2(this.count);
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    final flag = useState(false);
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ...List.generate(
+              count,
+              (index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Switch(
+                  value: flag.value,
+                  onChanged: (v) => flag.value = v,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SwitchTest3 extends HookWidget {
+  const _SwitchTest3(this.count);
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    final flag = useState(false);
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ...List.generate(
+              count,
+              (index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CupertinoSwitch(
+                  value: flag.value,
+                  onChanged: (v) => flag.value = v,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/components/form/switch.dart
+++ lib/src/components/form/switch.dart
@@ -1,18 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_element_ui/flutter_element_ui.dart';
+import 'package:flutter_element_ui/src/extension.dart';
 import 'package:luoyi_flutter_base/luoyi_flutter_base.dart';
+
+import '../basic/icon.dart';
 
 class ElSwitch extends StatefulWidget {
   const ElSwitch({
     super.key,
     required this.value,
     this.onChanged,
-    this.height = 24,
+    this.size = 20.0,
+    this.width,
+    this.gap = 4.0,
+    this.activeColor,
+    this.inactiveColor,
+    this.activeBgColor,
+    this.inactiveBgColor,
+    this.activeText,
+    this.inactiveText,
+    this.activeIcon,
+    this.inactiveIcon,
+    this.disabled = false,
   });
 
+  /// 开关状态
   final bool value;
+
+  /// change事件
   final ValueChanged<bool>? onChanged;
-  final double height;
+
+  /// 开关尺寸
+  final double size;
+
+  /// 自定义开关宽度，默认为[size]两倍
+  final double? width;
+
+  /// 开关与容器之间的间距
+  final double gap;
+
+  /// 开关激活颜色，默认白色
+  final Color? activeColor;
+
+  /// 开关未激活颜色，默认白色
+  final Color? inactiveColor;
+
+  /// 开关激活背景颜色，默认主题色
+  final Color? activeBgColor;
+
+  /// 开关未激活背景颜色，默认边框颜色
+  final Color? inactiveBgColor;
+
+  /// 开关激活文字（待实现）
+  final String? activeText;
+
+  /// 开关未激活文字（待实现）
+  final String? inactiveText;
+
+  /// 开关激活图标（待实现）
+  final ElIcon? activeIcon;
+
+  /// 开关未激活图标（待实现）
+  final ElIcon? inactiveIcon;
+
+  /// 是否禁用
+  final bool disabled;
 
   @override
   State<ElSwitch> createState() => _ElSwitchState();
@@ -21,25 +72,35 @@ class ElSwitch extends StatefulWidget {
 class _ElSwitchState extends State<ElSwitch>
     with SingleTickerProviderStateMixin {
   late final AnimationController controller;
-  late Animation<Offset> animation;
+  late Animation<double> animation;
+  bool _isInitial = true;
+
+  double get containerHeight => widget.size + widget.gap;
+
+  double get containerWidth => widget.width ?? containerHeight * 2;
+
+  double get disabledOpacity => widget.disabled ? 0.5 : 1.0;
+
+  Color get activeColor => widget.activeColor ?? Colors.white;
+
+  Color get inactiveColor => widget.inactiveColor ?? Colors.white;
+
+  Color get activeBgColor => (widget.activeBgColor ?? context.elTheme.primary)
+      .withOpacity(disabledOpacity);
+
+  Color get inactiveBgColor =>
+      (widget.inactiveBgColor ?? context.elTheme.borderColor)
+          .withOpacity(disabledOpacity);
 
   @override
   void initState() {
     super.initState();
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 200),
     );
-    animation = Tween(
-      begin: Offset(
-        -(widget.height / 2),
-        0,
-      ),
-      end: Offset(
-        -(widget.height / 2),
-        0,
-      ),
-    ).animate(controller);
+    final offset = containerWidth / 2 - containerHeight / 2;
+    animation = Tween(begin: -offset, end: offset).animate(controller);
   }
 
   @override
@@ -50,42 +111,46 @@ class _ElSwitchState extends State<ElSwitch>
 
   @override
   Widget build(BuildContext context) {
-    final elTheme = context.elTheme;
-    final $switchSize = widget.height - 4;
+    if (_isInitial) {
+      _isInitial = false;
+    } else {
+      widget.value ? controller.forward() : controller.reverse();
+    }
     return GestureDetector(
       onTap: () {
-        if (widget.onChanged != null) {
+        if (widget.onChanged != null && !widget.disabled) {
           widget.onChanged!(!widget.value);
-          !widget.value ? controller.forward : controller.reverse();
         }
       },
-      child: HoverBuilder(builder: (isHover) {
-        return AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          height: widget.height,
-          width: widget.height * 2,
-          decoration: BoxDecoration(
-            color: widget.value ? elTheme.primary : elTheme.borderColor,
-            borderRadius: BorderRadius.circular(widget.height / 2),
-          ),
-          child: SlideTransition(
-            position: animation,
-            child: UnconstrainedBox(
-              child: buildSwitch($switchSize),
+      child: HoverBuilder(
+        disabled: widget.disabled,
+        builder: (isHover) {
+          return AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            height: containerHeight,
+            width: containerWidth,
+            decoration: BoxDecoration(
+              color: widget.value ? activeBgColor : inactiveBgColor,
+              borderRadius: BorderRadius.circular(containerHeight / 2),
             ),
-          ),
-        );
-      }),
-    );
-  }
-
-  Widget buildSwitch(double size) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(size / 2),
+            child: AnimatedBuilder(
+              animation: animation,
+              builder: (context, child) => Transform.translate(
+                offset: Offset(animation.value, 0),
+                child: UnconstrainedBox(
+                  child: Container(
+                    width: widget.size,
+                    height: widget.size,
+                    decoration: BoxDecoration(
+                      color: widget.value ? activeColor : inactiveColor,
+                      borderRadius: BorderRadius.circular(widget.size / 2),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
