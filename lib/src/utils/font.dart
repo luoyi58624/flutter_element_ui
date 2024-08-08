4627eea1acf40b27447b2cb6a7bb704a5a190e46
luoyi <luoyi58624@163.com>
2024-08-08 13:44:08 +0800

    chore: 同步ElFont

--- lib/src/utils/font.dart
+++ lib/src/utils/font.dart
@@ -5,23 +5,17 @@ class ElFont {
   ElFont._();
 
   /// 普通字重
-  static FontWeight normal = FontWeight.w400;
+  static FontWeight get normal => FontUtil.normal;
+
+  static set normal(FontWeight v) => FontUtil.normal = v;
 
   /// 中等字重
-  static FontWeight medium = FontWeight.w500;
+  static FontWeight get medium => FontUtil.medium;
+
+  static set medium(FontWeight v) => FontUtil.medium = v;
 
   /// 粗体字重
-  static FontWeight bold = FontWeight.w700;
+  static FontWeight get bold => FontUtil.bold;
 
-  /// 字体回退列表，flutter会根据此列表依次匹配字体
-  static List<String>? get fontFamilyFallback {
-    // 在 mac 上若不指定苹方字体，那么中文字重只有两种
-    if (PlatformUtil.isMacOS || PlatformUtil.isIOS) {
-      return ['.AppleSystemUIFont', 'PingFang SC'];
-    } else if (PlatformUtil.isWindows) {
-      return ['Microsoft YaHei', '微软雅黑'];
-    } else {
-      return null;
-    }
-  }
+  static set bold(FontWeight v) => FontUtil.bold = v;
 }
