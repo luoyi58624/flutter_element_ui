73f85dea24798a98984cc146ac33792ebfa30e2b
luoyi <luoyi58624@163.com>
2024-08-08 11:51:58 +0800

    chore: 移除部署web页面脚本

--- script/build_example_web.sh
+++
@@ -1,6 +0,0 @@
-flutter pub get
-# shellcheck disable=SC2164
-cd ./example
-#flutter build web --wasm --pwa-strategy none --base-href /flutter_element_ui/
-flutter build web --web-renderer=html --pwa-strategy none --base-href /flutter_element_ui/
-#flutter build web --web-renderer=canvaskit --pwa-strategy none --base-href /flutter_element_ui/
