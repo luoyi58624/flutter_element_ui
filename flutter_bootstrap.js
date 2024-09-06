dd212e4e0400a2062cf10df90004f2a83db33b20
luoyi58624 <46178521+luoyi58624@users.noreply.github.com>
2024-09-06 06:15:22 +0000

    Deploying to web_page from @ luoyi58624/flutter_element_ui@41c332926c2bbe04227442cb815519133f16d02d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3311348818"
+    serviceWorkerVersion: "2298469859"
   }
 });
