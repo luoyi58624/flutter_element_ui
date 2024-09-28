3e40a26c14c6561834372e2e5b351398e3dc8597
luoyi <luoyi58624@gmail.com>
2024-09-28 22:59:07 +0800

    fix: 修复 Click 无法阻止更远祖先事件

--- packages/flutter_element_extension/lib/src/widgets/click.dart
+++ packages/flutter_element_extension/lib/src/widgets/click.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_element_extension/flutter_element_extension.dart';
+import 'package:flutter_element_ui/flutter_element_ui.dart';
 
-/// 点击事件小部件，它与 [GestureDetector] 不同在于，即使手势竞争失败也依然会触发点击事件，
-/// 同时，你可以
 class Click extends StatefulWidget {
+  /// 点击事件小部件，该小部件完全照抄 Web 的事件逻辑，它与 [GestureDetector] 不同在于，
+  /// 父组件的 [GestureDetector] 会被子组件的 [GestureDetector] 默认阻止，
+  /// 而 [Click] 则不同，当子组件的点击事件触发时，父组件也会触发点击事件，
+  /// 同时，你可以通过 [stopPropagation] 静态方法阻止祖先默认的点击事件。
+  ///
+  /// 应用场景：当我们希望父组件的点击事件不被子组件的点击事件所覆盖时，[Click] 小部件将非常有用，
+  /// 例如 [ElLink] 链接小部件，即使你包裹了 button 之类带有默认点击事件的小部件，
+  /// 你依然可以直接触发默认的链接跳转，若你希望阻止这一行为，只需要在点击事件中添加
+  /// Click.stopPropagation(context) 函数即可。
   const Click({
     super.key,
     required this.onClick,
@@ -14,9 +22,9 @@ class Click extends StatefulWidget {
   final VoidCallback onClick;
   final Widget child;
 
-  /// 根据上下文阻止事件冒泡
+  /// 根据上下文阻止事件冒泡，它会阻止祖先所有 [Click] 的点击事件
   static void stopPropagation(BuildContext context) {
-    _ClickInheritedWidget.of(context).stopPropagation();
+    _ClickInheritedWidget._stopPropagation(context);
   }
 
   @override
@@ -27,13 +35,13 @@ class _ClickState extends State<Click> {
   bool _flag = true;
 
   void _onTap() {
-    if (_flag) {
-      widget.onClick();
-    }
+    if (_flag) widget.onClick();
   }
 
   void _stopPropagation() {
     _flag = false;
+    // 依次阻止祖先所有 Click 点击事件
+    _ClickInheritedWidget._stopPropagation(context);
   }
 
   void _reset() {
@@ -89,11 +97,12 @@ class _ClickInheritedWidget extends InheritedWidget {
 
   final VoidCallback stopPropagation;
 
-  static _ClickInheritedWidget of(BuildContext context) {
+  static void _stopPropagation(BuildContext context) {
     final _ClickInheritedWidget? result =
         context.dependOnInheritedWidgetOfExactType<_ClickInheritedWidget>();
-    assert(result != null, 'No _ClickInheritedWidget found in context');
-    return result!;
+    if (result != null) {
+      result.stopPropagation();
+    }
   }
 
   @override
