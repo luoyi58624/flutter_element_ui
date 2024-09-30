581ef3f47050d32886cad08c144e7d0b769da0a2
luoyi58624 <46178521+luoyi58624@users.noreply.github.com>
2024-09-30 10:31:33 +0000

    Deploying to web_page from @ luoyi58624/flutter_element_ui@52a3976c8b7bbd75fea2bc54a87a543083a7bdde 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.json": "8fc22455836cdc4da3698721eeb6bce
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "06072487e16a841ae6d31ab3c77306af",
+"flutter_bootstrap.js": "b08c2f971ec633c8325422d8130bdd5d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "c0b474b322a0a6b6d4ff22a1a1697a6e",
