dbd0df5bc8d7c2f417cd1de686e957aab109c335
luoyi <luoyi58624@163.com>
2024-10-11 17:34:07 +0800

    chore: 移除旧版 example 文件夹

--- example/lib/global.dart
+++
@@ -1,8 +0,0 @@
-export 'package:flutter_base/flutter_base.dart';
-export 'package:flutter_element_ui/flutter_element_ui.dart';
-export 'package:flutter_element_extension/flutter_element_extension.dart';
-
-export 'http.dart';
-export 'state.dart';
-export 'router.dart';
-export 'hooks/simple_hook.dart';
