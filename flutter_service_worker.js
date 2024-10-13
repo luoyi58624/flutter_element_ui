13259a95ffde486fa537983c77a56cd73f885555
luoyi58624 <46178521+luoyi58624@users.noreply.github.com>
2024-10-13 15:35:13 +0000

    Deploying to web_page from @ luoyi58624/flutter_element_ui@5f2b2abbb5954db1c1eb4b46d5d24cd2beccdb5f 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "assets/assets/images/home/3.png": "d584595bb21b8813297de56a8398e393",
 "assets/assets/images/home/1.png": "f7440b611186b5148281177c4139af16",
 "assets/assets/images/home/theme.png": "c38b733c511e916e2fe40380358d617a",
-"assets/NOTICES": "c4f2e8708bfcb11e9a811ccd5a11b879",
+"assets/NOTICES": "3fa414c35ffea1e727704eef45e6203e",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "37ff41637dc91c08a23e471c502119e8",
 "assets/AssetManifest.bin.json": "16d094d70ad558c72fef053855ef44bf",
@@ -50,7 +50,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "137332a004e7ad68e9503dbd3baceadd",
-"flutter_bootstrap.js": "cfda61699aa410c3cc54dae5796fed64"};
+"flutter_bootstrap.js": "395eef653fbd9cf84345e1acb6bfa84f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
