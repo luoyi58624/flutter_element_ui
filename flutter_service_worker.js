c3b261e3a9a4ea195e3294fc6fa30b33aeaa5c1c
luoyi58624 <46178521+luoyi58624@users.noreply.github.com>
2024-10-13 14:14:55 +0000

    Deploying to web_page from @ luoyi58624/flutter_element_ui@7a7aa52a10a527f9faed082fce51db49f1ca3c4a 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -50,7 +50,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "137332a004e7ad68e9503dbd3baceadd",
-"flutter_bootstrap.js": "dde1cef9e1dba841b2453069dc0c17f5"};
+"flutter_bootstrap.js": "578bcf1c8139c392f8c744a1d863449a"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
