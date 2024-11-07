0d7557525bca76ed28ebdef0a168d2ab155203b2
luoyi <luoyi58624@gmail.com>
2024-11-07 22:29:57 +0800

    chore: 修复惯性滚动示例

--- element_docs/lib/pages/test/friction_animate.dart
+++ element_docs/lib/pages/test/friction_animate.dart
@@ -13,53 +13,13 @@ class FrictionAnimateTest extends StatefulWidget {
 }
 
 class _FrictionAnimateTestState extends State<FrictionAnimateTest> {
-  late final AnimationController controller =
-      AnimationController.unbounded(vsync: vsync)..addListener(listener);
-  final isActive = Obs(false);
-  final offset = Obs(const Offset(100, 100));
-
-  late BoxConstraints constraints;
-
-  /// 坐标距离
-  double distance = 0.0;
-
-  /// 坐标方向
-  double direction = 0.0;
-
-  double? oldValue;
-
-  void listener() {
-    if (oldValue == null) {
-      oldValue = controller.value;
-      distance += controller.value;
-    } else {
-      final details = controller.value - oldValue!;
-      i(details);
-      distance += details;
-      oldValue = controller.value;
-    }
-    offset.value = Offset.fromDirection(
-      direction,
-      distance,
-    ).clampConstraints(constraints);
-    // direction = offset.value.direction;
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    double size = 50;
     return Padding(
       padding: const EdgeInsets.all(50.0),
       child: SizedBox(
         height: 300,
-        child: LayoutBuilder(builder: (context, $constraints) {
-          constraints = $constraints;
+        child: LayoutBuilder(builder: (context, constraints) {
           return Stack(
             children: [
               Container(
@@ -69,59 +29,9 @@ class _FrictionAnimateTestState extends State<FrictionAnimateTest> {
               const Positioned(
                 child: _Demo(),
               ),
-              ObsBuilder(builder: (context) {
-                return Positioned(
-                  left: clampDouble(
-                    offset.value.dx,
-                    0,
-                    constraints.maxWidth - size,
-                  ),
-                  top: clampDouble(
-                    offset.value.dy,
-                    0,
-                    constraints.maxHeight - size,
-                  ),
-                  child: GestureDetector(
-                    onPanStart: (e) {
-                      controller.stop();
-                      oldValue = null;
-                      isActive.value = true;
-                    },
-                    onPanUpdate: (e) {
-                      offset.value += e.delta;
-                    },
-                    onPanEnd: (e) {
-                      isActive.value = false;
-                      // offset.value = offset.value.clampConstraints(constraints);
-
-                      i(e.velocity.pixelsPerSecond.direction);
-                      final dragOffset = offset.value;
-                      direction = e.velocity.pixelsPerSecond.direction;
-                      distance = dragOffset.distance;
-
-                      FrictionSimulation frictionSim = FrictionSimulation(
-                        0.015,
-                        0.0,
-                        e.velocity.pixelsPerSecond.distance,
-                      );
-                      controller.animateWith(frictionSim);
-                    },
-                    onPanCancel: () {
-                      isActive.value = false;
-                    },
-                    child: Material(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(size / 2),
-                      clipBehavior: Clip.hardEdge,
-                      elevation: isActive.value ? 4 : 0,
-                      child: SizedBox(
-                        width: size,
-                        height: size,
-                      ),
-                    ),
-                  ),
-                );
-              }),
+              _Demo2(
+                constraints: constraints,
+              ),
             ],
           );
         }),
@@ -131,7 +41,7 @@ class _FrictionAnimateTestState extends State<FrictionAnimateTest> {
 }
 
 class _Demo extends StatefulWidget {
-  const _Demo({super.key});
+  const _Demo();
 
   @override
   State<_Demo> createState() => _DemoState();
@@ -139,8 +49,9 @@ class _Demo extends StatefulWidget {
 
 class _DemoState extends State<_Demo> {
   Offset dragStartOffset = Offset.zero;
-  double direction = 0.0;
-  double distance = 0.0;
+  late Offset offset;
+  double direction = 1.0;
+  double distance = 100.0;
 
   @override
   Widget build(BuildContext context) {
@@ -149,15 +60,17 @@ class _DemoState extends State<_Demo> {
       offset: Offset.fromDirection(direction, distance),
       child: GestureDetector(
         onPanStart: (e) {
-          dragStartOffset = Offset.fromDirection(direction, distance);
+          offset = Offset.fromDirection(direction, distance);
+          dragStartOffset = e.globalPosition;
         },
         onPanUpdate: (e) {
           final dragOffset = e.globalPosition - dragStartOffset;
-          i(dragOffset.direction);
-          // setState(() {
-          //   direction = dragOffset.direction;
-          //   distance = dragOffset.distance;
-          // });
+          final targetOffset = offset + dragOffset;
+          i(targetOffset.direction);
+          setState(() {
+            direction = targetOffset.direction;
+            distance = targetOffset.distance;
+          });
         },
         child: Material(
           color: Colors.green,
@@ -170,5 +83,105 @@ class _DemoState extends State<_Demo> {
         ),
       ),
     );
+  }
+}
+
+class _Demo2 extends StatefulWidget {
+  const _Demo2({required this.constraints});
+
+  final BoxConstraints constraints;
+
+  @override
+  State<_Demo2> createState() => _Demo2State();
+}
+
+class _Demo2State extends State<_Demo2> {
+  late final AnimationController controller =
+      AnimationController.unbounded(vsync: vsync)..addListener(listener);
+  final isActive = Obs(false);
+  final offset = Obs(const Offset(100, 100));
+
+  /// 坐标方向
+  double direction = 0.0;
+
+  double? oldValue;
+
+  void listener() {
+    late double details;
+    if (oldValue == null) {
+      oldValue = controller.value;
+      details = controller.value;
+      // distance += controller.value;
+    } else {
+      details = controller.value - oldValue!;
+      oldValue = controller.value;
+      // distance += details;
+    }
+    i(Offset.fromDirection(direction, details));
+    offset.value = (offset.value + Offset.fromDirection(direction, details))
+        .clampConstraints(widget.constraints);
+    // offset.value = Offset.fromDirection(
+    //   direction,
+    //   distance,
+    // ).clampConstraints(widget.constraints);
+    // direction = offset.value.direction;
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double size = 50;
+    return ObsBuilder(builder: (context) {
+      return Positioned(
+        left: clampDouble(
+          offset.value.dx,
+          0,
+          widget.constraints.maxWidth - size,
+        ),
+        top: clampDouble(
+          offset.value.dy,
+          0,
+          widget.constraints.maxHeight - size,
+        ),
+        child: GestureDetector(
+          onPanStart: (e) {
+            controller.stop();
+            isActive.value = true;
+            oldValue = null;
+          },
+          onPanUpdate: (e) {
+            offset.value += e.delta;
+          },
+          onPanEnd: (e) {
+            isActive.value = false;
+            direction = e.velocity.pixelsPerSecond.direction;
+            FrictionSimulation frictionSim = FrictionSimulation(
+              0.01,
+              0.0,
+              e.velocity.pixelsPerSecond.distance,
+            );
+            controller.animateWith(frictionSim);
+          },
+          onPanCancel: () {
+            isActive.value = false;
+          },
+          child: Material(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(size / 2),
+            clipBehavior: Clip.hardEdge,
+            elevation: isActive.value ? 4 : 0,
+            child: SizedBox(
+              width: size,
+              height: size,
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
