9dce4e36055528994df39f3477c0037628de5477
luoyi <luoyi58624@gmail.com>
2024-12-01 10:23:08 +0800

    chore: 更新 github 脚本

--- scripts/web_page.sh
+++ scripts/web_page.sh
@@ -1,6 +1,6 @@
 flutter --version
 # shellcheck disable=SC2164
-cd ./element_docs
+cd ./packages/element_docs
 flutter pub upgrade
 flutter build web --web-renderer=html --base-href /flutter_element_ui/
 #flutter build web --web-renderer=canvaskit --pwa-strategy none --base-href /flutter_element_ui/
