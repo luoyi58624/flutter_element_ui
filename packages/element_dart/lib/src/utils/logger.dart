8df6a7e28bd27c0d7f472eed69de9a3b25baa8fc
luoyi <luoyi58624@163.com>
2024-12-20 15:36:36 +0800

    chore: 调整全局 logger 参数

--- packages/element_dart/lib/src/utils/logger.dart
+++ packages/element_dart/lib/src/utils/logger.dart
@@ -2,58 +2,44 @@ import 'package:logger/logger.dart';
 
 import 'common.dart';
 
-PrettyPrinter _prettyPrinter = PrettyPrinter(
-  methodCount: 2,
-  errorMethodCount: 2,
-  printEmojis: false,
-  noBoxingByDefault: false,
+/// 全局日志打印对象，默认情况下，在开发环境会打印所有级别日志，生产环境则打印警告级别以上日志
+Logger logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 2,
+    errorMethodCount: 2,
+    printEmojis: false,
+    noBoxingByDefault: false,
+  ),
+  filter: _LoggerFilter(loggerLevel: isRelease ? Level.warning : Level.all),
 );
-
-var _loggerInstance = Logger(
-  printer: _prettyPrinter,
-  filter: _LoggerFilter(loggerLevel: null),
-);
-
-class _LoggerFilter extends LogFilter {
-  final Level? loggerLevel;
-
-  _LoggerFilter({this.loggerLevel});
-
-  @override
-  bool shouldLog(LogEvent event) {
-    if (loggerLevel == null) {
-      return !isRelease;
-    } else {
-      return event.level.value >= loggerLevel!.value;
-    }
-  }
-}
-
-/// 设置日志打印级别，默认情况下，在开发环境会打印所有级别日志，生产环境则屏蔽所有日志
-void setLogger({Level? level, Logger? logger}) {
-  _loggerInstance = logger ??
-      Logger(
-        printer: _prettyPrinter,
-        filter: _LoggerFilter(loggerLevel: level),
-      );
-}
 
 /// debug 级别日志输出
 void d(dynamic message, [dynamic title]) {
-  _loggerInstance.d(message, error: title);
+  logger.d(message, error: title);
 }
 
 /// info 级别日志输出
 void i(dynamic message, [dynamic title]) {
-  _loggerInstance.i(message, error: title);
+  logger.i(message, error: title);
 }
 
 /// warning 级别日志输出
 void w(dynamic message, [dynamic title]) {
-  _loggerInstance.w(message, error: title);
+  logger.w(message, error: title);
 }
 
 /// error 级别日志输出
 void e(dynamic message, [dynamic title]) {
-  _loggerInstance.e(message, error: title);
+  logger.e(message, error: title);
+}
+
+class _LoggerFilter extends LogFilter {
+  final Level loggerLevel;
+
+  _LoggerFilter({required this.loggerLevel});
+
+  @override
+  bool shouldLog(LogEvent event) {
+    return event.level.value >= loggerLevel.value;
+  }
 }
